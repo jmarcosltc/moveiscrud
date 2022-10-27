ca5163d47f970a1778c19b9e5857afc46d51ee6b
unknown <joao163marcos@gmail.com>
2022-10-27 11:23:17 -0300

    rotas 0.3.0-a.1

--- src/backend/routes.ts
+++ src/backend/routes.ts
@@ -1,13 +1,13 @@
-// Importar o mysql2
+// Importar
 import mysql from 'mysql2';
 import express, { Request, Response, NextFunction, Express} from 'express';
-// Importar o router
+// Importar o roteador do express
 const router = express.Router();
 
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'dbuser',
-  password : 's3kreee7'
+  user     : '',
+  password : ''
 });
 
 connection.connect();
@@ -33,21 +33,61 @@ ambiente
 // rota para criar um movel novo
 router.post('/moveis', (req: Request, res: Response) => {
     // query para adicionar um móvel
+    const { nome, imagem, preco, ambiente } = req.body
+    connection.query(
+        'INSERT INTO `moveis` (nome, imagem, preco, ambiente) ' + `VALUES (${nome}, ${imagem}, ${preco}, ${ambiente})`,
+        function(err, results, fields) {
+          console.log(results); // reseultados
+          console.log(fields); // não sei ainda
+          return res.status(200).send()
+        }
+      );
 })
 
 // rota pra mostrar os moveis
 router.get('/moveis', (req: Request, res: Response) => {
     // query para pegar todos os moveis
+    connection.query(
+        'SELECT * FROM `moveis`',
+        function(err, results, fields) {
+          console.log(results); // reseultados
+          console.log(fields); // não sei ainda
+          return res.status(200).send(results);
+        }
+      );
 })
 
 // rota pra editar um movel
 router.put('/moveis:id', (req: Request, res: Response) => {
     // query para pegar todos os moveis
+    const { nome, imagem, preco, ambiente } = req.body
+    connection.query(
+        'UPDATE `moveis` ' + 
+        'SET `nome` = ' + `${nome} ` +
+        '`imagem` = ' + `${imagem} ` +
+        '`preco` = ' + `${preco} ` +
+        '`ambiente` = ' + `${ambiente} ` +
+        'WHERE `id` ' + `= ${req.params.id}`,
+        function(err, results, fields) {
+          console.log(results); // reseultados
+          console.log(fields); // não sei ainda
+          return res.status(200).send()
+        }
+      );
 })
 
 // rota pra remover um movel
 router.delete('/moveis/:id', (req: Request, res: Response) => {
     // query para pegar todos os moveis
+    connection.query(
+        'DELETE FROM `moveis` ' +
+        'WHERE `id` ' + `= ${req.params.id}`,
+        function(err, results, fields) {
+          console.log(results); // reseultados
+          console.log(fields); // não sei ainda
+          return res.status(200).send()
+        }
+      );
 })
 
 connection.end();
--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -1,3 +1,52 @@
 // http requests pelo axios para a API
+axios.post('/moveis', {
+    data: {
+        nome: nome,
+        imagem: imagem,
+        preco: preco,
+        ambiente: ambiente
+    }
+  })
+  .then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.log(error);
+});
+
+axios.put('/moveis', {
+    params: {
+        id: id
+    },
+    data: {
+        nome: nome,
+        imagem: imagem,
+        preco: preco,
+        ambiente: ambiente
+    }
+  })
+  .then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.log(error);
+});
+
+axios.delete('/moveis', {
+    params: {
+        id: id
+    }
+  })
+  .then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.log(error);
+});
+
+axios.get('/moveis', {})
+  .then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.log(error);
+});
+
 
 // sistema de ordenação
