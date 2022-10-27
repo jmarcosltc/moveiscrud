46c6b881be1d0c8cb52d41523453de2a906e8990
unknown <joao163marcos@gmail.com>
2022-10-27 10:24:29 -0300

    inicio

---
+++ src/backend/backend.ts
@@ -0,0 +1,1 @@
+// Aqui vai a configuração do express, como por exemplo a porta, host e cors
---
+++ src/backend/routes.ts
@@ -0,0 +1,15 @@
+/*
+id
+nome
+imagem
+preço
+ambiente
+*/
+
+// rota para criar um movel novo
+
+// rota pra mostrar os moveis
+
+// rota pra editar um movel
+
+// rota pra remover um movel
